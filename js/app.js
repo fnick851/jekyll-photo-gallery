ffada2d0a383d9a9db1696505ab55e1bdcdf3299
(Noah) Hanlun Song <hsong@air.org>
2018-10-16 18:54:40 -0400

    fade in image to get around a chrome behavior?

--- js/app.js
+++ js/app.js
@@ -2,9 +2,14 @@
     "use strict";
 
     // fade in images
-    $('.fadedIn-image').on('load', function() {
-        $(this).css('opacity','1');
+    $('.fadedIn-image').on('load', function () {
+        $(this).css('opacity', '1');
         $(this).parent().parent().parent().find('p') ? $(this).parent().parent().parent().find('p').fadeIn(1600) : null;
     });
+
+    setTimeout(function () {
+        $('.fadedIn-image').css('opacity', '1');
+        $('.fadedIn-image').parent().parent().parent().find('p') ? $(this).parent().parent().parent().find('p').fadeIn(1600) : null;
+    }, 2000);
 
 })();
