77b4a29ecb6b3b7e5a736d5f4ac20184d9f8ffa2
(Noah) Hanlun Song <hsong@air.org>
2018-10-16 18:40:38 -0400

    fine tune fade in effect

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@
     "use strict";
 
     // fade in images
-    $('.fadedIn-image').fadeIn(1500);
-    $('.fadedIn-image').parent().parent().parent().find('p') ? $('img').parent().parent().parent().find('p').fadeIn(1600) : null;
+    $('.fadedIn-image').on('load', function() {
+        $(this).css('opacity','1');
+        $(this).parent().parent().parent().find('p') ? $(this).parent().parent().parent().find('p').fadeIn(1600) : null;
+    });
 
 })();
